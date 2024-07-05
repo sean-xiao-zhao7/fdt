026be1e100d66cc1d6dbc0f0302551fc9faf4dbb
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-07-05 13:41:05 -0400

    Add dark mode switcher

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,21 +1,75 @@
+import { createContext, useContext, useState, useMemo } from "react";
 import { Outlet } from "react-router-dom";
+
+import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Box from "@mui/material/Box";
 
-export default function MainLayout() {
+const ColorModeContext = createContext({ toggleColorMode: () => {} });
+
+const MainLayout = function () {
+    const theme = useTheme();
+    const colorMode = useContext(ColorModeContext);
+
     return (
         <Box sx={{ width: "100%", maxWidth: 500 }}>
             <ButtonGroup
                 variant="contained"
                 size="large"
-                aria-label="Large button group"
+                aria-label="Main menu"
             >
                 <Button>Dashboard</Button>
                 <Button>Data Table</Button>
+                <IconButton
+                    sx={{ ml: 1 }}
+                    onClick={colorMode.toggleColorMode}
+                    color="inherit"
+                >
+                    {theme.palette.mode === "dark" ? (
+                        <Brightness7Icon />
+                    ) : (
+                        <Brightness4Icon />
+                    )}
+                </IconButton>
             </ButtonGroup>
             <Outlet />
         </Box>
+    );
+};
+
+export default function ToggleColorMode() {
+    const [mode, setMode] = useState<"light" | "dark">("light");
+    const colorMode = useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) =>
+                    prevMode === "light" ? "dark" : "light"
+                );
+            },
+        }),
+        []
+    );
+
+    const theme = useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode,
+                },
+            }),
+        [mode]
+    );
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <MainLayout />
+            </ThemeProvider>
+        </ColorModeContext.Provider>
     );
 }
