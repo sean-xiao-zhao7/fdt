6604f3b245916e9a953ca48442ec18498fe89d67
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-07-16 16:08:52 -0400

    Add provider to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-
-import { router } from "./router/router";
+import { Provider } from "react-redux";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -10,10 +9,14 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 import "./styles/index.scss";
+import { router } from "./router/router";
+import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
     </React.StrictMode>
 );
