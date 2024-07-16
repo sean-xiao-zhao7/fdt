fb4055199081fb27a69ae5c4d99177a8e25bd730
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-07-16 16:06:02 -0400

    Use map on articles on dashboard

--- src/components/pages/DashboardHomePage.tsx
+++ src/components/pages/DashboardHomePage.tsx
@@ -1,3 +1,5 @@
+import { useAppSelector } from "../../store/hooks";
+
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Divider from "@mui/material/Divider";
@@ -5,8 +7,22 @@ import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
+import { LinearProgress } from "@mui/material";
 
 export default function DashboardHomePage() {
+    const allArticles = useAppSelector((state) => state.articles.articles);
+
+    if (!allArticles) {
+        return <LinearProgress />;
+    } else if (allArticles.length <= 0) {
+        return (
+            <>
+                <Typography variant="h1">Dashboard</Typography>
+                <Typography>No articles.</Typography>
+            </>
+        );
+    }
+
     return (
         <>
             <Typography variant="h1">Dashboard</Typography>
@@ -16,33 +32,37 @@ export default function DashboardHomePage() {
                     bgcolor: "background.paper",
                 }}
             >
-                <Divider variant="inset" component="li" />
-                <ListItem alignItems="flex-start">
-                    <ListItemAvatar>
-                        <Avatar
-                            alt="Travis Howard"
-                            src="/static/images/avatar/2.jpg"
-                        />
-                    </ListItemAvatar>
-                    <ListItemText
-                        primary="Summer BBQ"
-                        secondary={
-                            <>
-                                <Typography
-                                    sx={{ display: "inline" }}
-                                    component="span"
-                                    variant="body2"
-                                    color="text.primary"
-                                >
-                                    to Scott, Alex, Jennifer
-                                </Typography>
-                                {
-                                    " — Wish I could come, but I'm out of town this…"
-                                }
-                            </>
-                        }
-                    />
-                </ListItem>
+                {allArticles.map((article) => {
+                    return (
+                        <>
+                            <Divider variant="inset" component="li" />
+                            <ListItem alignItems="flex-start">
+                                <ListItemAvatar>
+                                    <Avatar
+                                        alt={article.author}
+                                        src={article.imageURL}
+                                    />
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary={article.title}
+                                    secondary={
+                                        <>
+                                            <Typography
+                                                sx={{ display: "inline" }}
+                                                component="span"
+                                                variant="body2"
+                                                color="text.primary"
+                                            >
+                                                {article.intro}
+                                            </Typography>
+                                            {article.body}
+                                        </>
+                                    }
+                                />
+                            </ListItem>
+                        </>
+                    );
+                })}
             </List>
         </>
     );
