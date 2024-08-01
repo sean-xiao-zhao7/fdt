4206563be621ebf352647ac3bd602036435c3ed1
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-08-01 15:13:03 -0400

    Addd home page content.

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,9 +1,21 @@
+import { Divider, Paper } from "@mui/material";
 import Typography from "@mui/material/Typography";
 
 export default function HomePage() {
     return (
-        <>
-            <Typography variant="h1">Select from top</Typography>
-        </>
+        <Paper
+            elevation={2}
+            square
+            sx={{ padding: "2rem", textAlign: "center" }}
+        >
+            <Typography variant="h1">Welcome to Jamey's blog.</Typography>
+            <Divider sx={{ margin: "1rem 0" }} />
+            <Typography variant="body1">
+                Welcome to Jamey's Christian applications blog.
+            </Typography>
+            <Typography variant="body1">
+                Select topic you like from the top.
+            </Typography>
+        </Paper>
     );
 }
