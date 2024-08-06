43fe522a917deb5010d0aa4d409a3d499230c1aa
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-08-06 16:39:46 -0400

    Add addArticlePage

---
+++ src/components/pages/AddArticlePage.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import { Paper } from "@mui/material";
+
+import AddSingleArticle from "../dashboard/AddSingleArticle";
+
+import { useAppDispatch } from "../../store/hooks";
+import { addSingleArticle } from "../../store/slices/articlesSlice";
+
+export default function AddArticlePage() {
+    const dispatch = useAppDispatch();
+    const [addNewArticle, setAddNewArticle] = useState(false);
+
+    const openNewArticleModal = () => {
+        setAddNewArticle(true);
+    };
+
+    const closeNewArticleModal = () => {
+        setAddNewArticle(false);
+    };
+
+    const submitNewArticle = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const formJson = Object.fromEntries((formData as any).entries());
+        const title = formJson.title;
+        const intro = formJson.intro;
+        const body = formJson.body;
+        const author = "Travis Howard";
+        const dateAdded = Date.now().toLocaleString();
+        dispatch(
+            addSingleArticle({
+                id: Date.now().toLocaleString(),
+                title,
+                intro,
+                body,
+                author,
+                imageURL: "",
+                dateAdded,
+            })
+        );
+        closeNewArticleModal();
+    };
+    return (
+        <Paper elevation={2} square sx={{ padding: "2rem" }}>
+            <AddSingleArticle
+                open={addNewArticle}
+                handleClose={closeNewArticleModal}
+                handleSubmit={submitNewArticle}
+            />
+            <Fab
+                variant="extended"
+                color="primary"
+                aria-label="add"
+                onClick={openNewArticleModal}
+                sx={{ margin: "1rem 0" }}
+            >
+                <AddIcon sx={{ mr: 1 }} />
+                New Article
+            </Fab>
+        </Paper>
+    );
+}
--- src/components/pages/BasePage.tsx
+++ src/components/pages/BasePage.tsx
@@ -1,54 +1,15 @@
-import { useAppSelector, useAppDispatch } from "../../store/hooks";
-// import { useState } from "react";
+import { useAppSelector } from "../../store/hooks";
 
 import { LinearProgress, Paper } from "@mui/material";
 import List from "@mui/material/List";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-// import Fab from "@mui/material/Fab";
-// import AddIcon from "@mui/icons-material/Add";
 
 // custom components
 import SingleArticleListItem from "../dashboard/SingleArticleItem";
 
-// import AddSingleArticle from "../dashboard/AddSingleArticle";
-// import { addSingleArticle } from "../../store/slices/articlesSlice";
-
 export default function BasePage({ title, articleType }) {
     const allArticles = useAppSelector((state) => state.articles.articles);
-
-    // const dispatch = useAppDispatch();
-    // const [addNewArticle, setAddNewArticle] = useState(false);
-
-    // const openNewArticleModal = () => {
-    //     setAddNewArticle(true);
-    // };
-
-    // const closeNewArticleModal = () => {
-    //     setAddNewArticle(false);
-    // };
-
-    // const submitNewArticle = (event: React.FormEvent<HTMLFormElement>) => {
-    //     event.preventDefault();
-    //     const formData = new FormData(event.currentTarget);
-    //     const formJson = Object.fromEntries((formData as any).entries());
-    //     const title = formJson.title;
-    //     const intro = formJson.intro;
-    //     const body = formJson.body;
-    //     const author = "Travis Howard";
-    //     const dateAdded = Date.now().toLocaleString();
-    //     dispatch(
-    //         addSingleArticle({
-    //             title,
-    //             intro,
-    //             body,
-    //             author,
-    //             imageURL: "",
-    //             dateAdded,
-    //         })
-    //     );
-    //     closeNewArticleModal();
-    // };
 
     if (!allArticles) {
         return <LinearProgress />;
@@ -63,24 +24,9 @@ export default function BasePage({ title, articleType }) {
 
     return (
         <Paper elevation={2} square sx={{ padding: "2rem" }}>
-            {/* <AddSingleArticle
-                open={addNewArticle}
-                handleClose={closeNewArticleModal}
-                handleSubmit={submitNewArticle}
-            /> */}
             <Typography variant="h1" sx={{ marginLeft: "0.5rem" }}>
                 {title}
             </Typography>
-            {/* <Fab
-                variant="extended"
-                color="primary"
-                aria-label="add"
-                onClick={openNewArticleModal}
-                sx={{ margin: "1rem 0" }}
-            >
-                <AddIcon sx={{ mr: 1 }} />
-                New Article
-            </Fab> */}
             <List
                 sx={{
                     width: "100%",
