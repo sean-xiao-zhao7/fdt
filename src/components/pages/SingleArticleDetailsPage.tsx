1ca18ad808789ea32c5ae5d617fdb39b8d4a8308
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-08-07 16:28:16 -0400

    Display body in single article

--- src/components/pages/SingleArticleDetailsPage.tsx
+++ src/components/pages/SingleArticleDetailsPage.tsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useAppSelector } from "../../store/hooks";
+import { Paper, Typography } from "@mui/material";
 
 export default function SingleArticleDetailsPage() {
     const { id } = useParams();
@@ -7,5 +8,10 @@ export default function SingleArticleDetailsPage() {
         state.articles.articles.find((article) => id === article.id)
     );
 
-    return <>Single item page for {currentArticle.title}</>;
+    return (
+        <Paper sx={{ padding: "2rem" }} elevation={2}>
+            <Typography variant="h2">{currentArticle.title}</Typography>
+            <Typography variant="body1">{currentArticle.body}</Typography>
+        </Paper>
+    );
 }
