9a7e33f47b21eef570610f965fd5ef9defc7d711
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-08-19 16:26:27 -0400

    Add actions bar under article titles

--- src/components/pages/SingleArticleDetailsPage.tsx
+++ src/components/pages/SingleArticleDetailsPage.tsx
@@ -1,12 +1,22 @@
 import { useParams } from "react-router-dom";
 import { useAppSelector } from "../../store/hooks";
-import { Divider, Paper, Typography } from "@mui/material";
+import {
+    Divider,
+    Paper,
+    Typography,
+    Box,
+    Chip,
+    IconButton,
+} from "@mui/material";
+import { FavoriteSharp, ShareRounded } from "@mui/icons-material";
 
 export default function SingleArticleDetailsPage() {
     const { id } = useParams();
     const currentArticle = useAppSelector((state) =>
         state.articles.articles.find((article) => id === article.id)
     );
+
+    const navigatePage = () => {};
 
     return (
         <Paper sx={{ padding: "2rem 4rem" }} elevation={2}>
@@ -16,6 +26,21 @@ export default function SingleArticleDetailsPage() {
             >
                 {currentArticle.title}
             </Typography>
+            <Box textAlign={"center"} sx={{ marginTop: 1 }}>
+                <Chip
+                    label={currentArticle.type.toUpperCase()}
+                    variant="outlined"
+                    onClick={navigatePage}
+                    onDelete={navigatePage}
+                    sx={{ mr: 1 }}
+                />
+                <IconButton aria-label="favorite" color="primary">
+                    <FavoriteSharp />
+                </IconButton>
+                <IconButton aria-label="share" color="primary">
+                    <ShareRounded />
+                </IconButton>
+            </Box>
             <Divider component="div" sx={{ margin: "1rem 0" }} />
             <Typography variant="body1">{currentArticle.intro}</Typography>
             {currentArticle.body.map((paragraph, index) => {
